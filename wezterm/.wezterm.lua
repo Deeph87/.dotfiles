9b67ffac61ae34dc5273e774a5dc163560e59110
Deeph87 <jhaudry@proton.me>
2024-02-25 16:42:59 +0100

    feat: wezterm full screen at start up

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,11 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
