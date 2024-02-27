763a58942ee5e16f2fe31e053fbde7c6d44ca20f
Jérémy Haudry <jeremy.haudry@atinternet.com>
2024-02-27 01:52:33 +0100

    feat: nvim better folder name & improve wezterm config

--- neovim/lua/jh/core/init.lua
+++
@@ -1,2 +0,0 @@
-require('jh.core.keymaps')
-require('jh.core.options')
--- neovim/lua/jh/plugins/nvim-highlight-colors.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "brenoprata10/nvim-highlight-colors",
-  config = function ()
-    local nvimHighlightColors = require("nvim-highlight-colors");
-    require("nvim-highlight-colors").setup();
-    nvimHighlightColors.turnOn();
-  end
-}
