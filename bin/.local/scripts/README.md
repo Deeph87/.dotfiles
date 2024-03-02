b52ad63fdd3aefba033011642c25c9deb66d6744
Deeph87 <jhaudry@proton.me>
2024-03-02 23:48:27 +0100

    feat: add remaps

---
+++ bin/.local/scripts/README.md
@@ -0,0 +1,4 @@
+# Config
+```bash
+ln -s ~/dotfiles/bin/.local/scripts/tmux-sessionizer ~/.local/scripts/tmux-sessionizer
+```
--- nvim/lua/jh/core/keymaps.lua
+++ nvim/lua/jh/core/keymaps.lua
@@ -16,3 +16,18 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 -- oil
 vim.keymap.set("n", "-", function() require('oil').open_float() end, { desc = "Open oil float parent directory" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>yy", "\"+yy")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
