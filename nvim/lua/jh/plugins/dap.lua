ba9660b64558bb856358785b3c27a51f166ff9c6
Jérémy Haudry <jeremy.haudry@atinternet.com>
2024-03-05 18:01:40 +0100

    feat: try dap and add TPope vim commentary

--- nvim/lua/jh/plugins/dap.lua
+++ nvim/lua/jh/plugins/dap.lua
@@ -12,39 +12,39 @@ return {
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+    -- 'rcarriga/nvim-dap-ui',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    -- 'leoluz/nvim-dap-go',
     {
       "microsoft/vscode-js-debug",
       build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
       version = "1.*",
     },    -- Install the vscode-js-debug adapter
-    {
-      "mxsdev/nvim-dap-vscode-js",
-      config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("dap-vscode-js").setup({
+    --{
+      --"mxsdev/nvim-dap-vscode-js",
+      --config = function()
+        --@diagnostic disable-next-line: missing-fields
+        --require("dap-vscode-js").setup({
           -- Path of node executable. Defaults to $NODE_PATH, and then "node"
           -- node_path = "node",
 
           -- Path to vscode-js-debug installation.
           -- debugger_path = "/home/jeremy/vscode-js-debug",
-          debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+          --debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
 
           -- Command to use to launch the debug server. Takes precedence over "node_path" and "debugger_path"
           -- debugger_cmd = { "js-debug-adapter" },
 
           -- which adapters to register in nvim-dap
-          adapters = {
-            "chrome",
-            "pwa-node",
-            "pwa-chrome",
-            "pwa-msedge",
-            "pwa-extensionHost",
-            "node-terminal",
-          },
+          --adapters = {
+            --"chrome",
+            --"pwa-node",
+            --"pwa-chrome",
+            --"pwa-msedge",
+            --"pwa-extensionHost",
+            --"node-terminal",
+          --},
 
           -- Path for file logging
           -- log_file_path = "(stdpath cache)/dap_vscode_js.log",
@@ -54,55 +54,50 @@ return {
 
           -- Logging level for output to console. Set to false to disable console output.
           -- log_console_level = vim.log.levels.ERROR,
-        })
-      end,
-    }
+        --})
+      --end,
+    --}
   },
   config = function()
     local dap = require 'dap'
-    local dapui = require 'dapui'
-    local js_based_languages = {
-      "typescript", "javascript", "javascriptreact", "typescriptreact",
-      -- using pwa-chrome
-      "vue"
+    --local dapui = require 'dapui'
+    -- local js_based_languages = {
+      -- "typescript", "javascript"
+    -- }
+
+    dap.adapters["pwa-node"] = {
+      type = "executable",
+      command = "node "..vim.fn.resolve(vim.fn.stdpath("data")).."/lazy/vscode-js-debug/out/src/vsDebugServer.js",
+      name = "node"
     }
-    for _, language in ipairs(js_based_languages) do
-      dap.configurations[language] = {
-        {
-          type = 'pwa-node',
-          request = 'attach',
-          name = 'Auto Attach (pwa-node)',
-          cwd = vim.fn.getcwd(),
-          sourceMaps = true,
-          skipFiles = { '<node_internals>/**' },
-        },
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch Current File (pwa-node)',
-          cwd = vim.fn.getcwd(),
-          args = { '${file}' },
-          sourceMaps = true,
-          protocol = 'inspector',
-        },
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch Current File (pwa-node with ts-node)',
-          cwd = vim.fn.getcwd(),
-          runtimeArgs = { '--loader', 'ts-node/esm' },
-          runtimeExecutable = 'node',
-          args = { '${file}' },
-          sourceMaps = true,
-          protocol = 'inspector',
-          skipFiles = { '<node_internals>/**', 'node_modules/**' },
-          resolveSourceMapLocations = {
-            "${workspaceFolder}/**",
-            "!**/node_modules/**",
-          }
-        }
-      }
-    end
+
+    dap.configurations.javascript = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+    }
+
+    --for _, language in ipairs(js_based_languages) do
+      --dap.configurations[language] = {
+        --{
+          --type = 'node-terminal',
+          --request = 'launch',
+          --name = 'start local dev',
+          --command = 'npm run start:local:dev',
+        --},
+        --{
+          --type = "pwa-node",
+          --request = "launch",
+          --name = "Launch file",
+          --program = "${file}",
+          --cwd = "${workspaceFolder}",
+        --}
+      --}
+    -- end,
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
@@ -116,31 +111,31 @@ return {
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
+    --dapui.setup {
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+      --icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      --controls = {
+        --icons = {
+          --pause = '⏸',
+          --play = '▶',
+          --step_into = '⏎',
+          --step_over = '⏭',
+          --step_out = '⏮',
+          --step_back = 'b',
+          --run_last = '▶▶',
+          --terminate = '⏹',
+          --disconnect = '⏏',
+        --},
+      --},
+    --}
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    --vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    --dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    --dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    --dap.listeners.before.event_exited['dapui_config'] = dapui.close
   end,
 }
