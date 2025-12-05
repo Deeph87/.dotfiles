7d7d3e5a5adeed9ed69837c5b0b85df8529c9195
jeremy <jhaudry@proton.me>
2025-12-06 00:23:25 +0100

    update nvim and ghostty confs

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ Add terminal prompt customization
 ```bash
 stow --dir ~/.dotfiles --target ~ starship
 ```
+
+### ghostty
+Add terminal prompt customization
+```bash
+sdsd
+```
+
+### nvim
+Add nvim config
+```bash
+stow --dir ~/.dotfiles --target ~ nvim
+git clone git@github.com:nvim-lua/plenary.nvim.git ~/.config/nvim/pack/vendor/start/plenary.nvim
+git clone git@github.com:nvim-telescope/telescope.nvim.git ~/.config/nvim/pack/vendor/start/telescope.nvim
+git clone git@github.com:nvim-lualine/lualine.nvim.git ~/.config/nvim/pack/vendor/start/lualine.nvim
+git clone git@github.com:catppuccin/nvim.git ~/.config/nvim/pack/vendor/start/catppuccin.nvim
+# In nvim, load all tags
+:helptags ALL
+```
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,37 +1,65 @@
+-- LOAD PLUGINS
+local telescope = require('telescope.builtin')
+
+-- INIT PLUGINS
+require('lualine').setup {
+  options = { theme  = "catppuccin" }
+}
+
+require("catppuccin").setup {
+  flavour = "macchiato"
+}
+vim.cmd.colorscheme "catppuccin"
+
+-- OPTIONS
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.api.nvim_set_option("clipboard", "unnamed")
+vim.opt.number = true
 
-require("jh.core")
-require("jh.lazy")
+-- KEYMAPS
+vim.keymap.set('n', '<leader>ge', vim.cmd.Ex)
+-- telescope
+vim.keymap.set('n', '<leader>gn', telescope.find_files, {})
+vim.keymap.set('n', '<leader>ls', telescope.buffers, {})
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+-- vim.pack.add{
+--   { src = 'https://github.com/nvim-lua/plenary.nvim' },
+--   { src = 'https://github.com/nvim-telescope/telescope.nvim' }
+-- }l
 
--- Dynamic make file program selector
-local function match(file, pattern)
-  local startPosition, endPosition = string.find(file, pattern)
-  return startPosition ~= nil and endPosition ~= nil
-end
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "typescript",
-  callback = function(ev)
-    local file = ev.file
-    -- ts tests files
-    if match(file, "%.spec%.") or match(file, "%.test%.") then
-      print("TS test file : " .. file)
-      vim.cmd("compiler jest | setlocal makeprg=npx\\ jest")
-    else
-      vim.cmd("compiler tsc | setlocal makeprg=npx\\ tsc")
-    end
-  end
-})
+
+-- vim.api.nvim_set_option("clipboard", "unnamed")
+
+-- require("jh.core")
+-- require("jh.lazy")
+
+-- -- [[ Highlight on yank ]]
+-- -- See `:help vim.highlight.on_yank()`
+-- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+--   group = highlight_group,
+--   pattern = '*',
+-- })
+
+-- -- Dynamic make file program selector
+-- local function match(file, pattern)
+--   local startPosition, endPosition = string.find(file, pattern)
+--   return startPosition ~= nil and endPosition ~= nil
+-- end
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "typescript",
+--   callback = function(ev)
+--     local file = ev.file
+--     -- ts tests files
+--     if match(file, "%.spec%.") or match(file, "%.test%.") then
+--       print("TS test file : " .. file)
+--       vim.cmd("compiler jest | setlocal makeprg=npx\\ jest")
+--     else
+--       vim.cmd("compiler tsc | setlocal makeprg=npx\\ tsc")
+--     end
+--   end
+-- })
 
