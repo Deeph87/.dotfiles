b73cbd25e78ea376a1dc2a467294992c2723fc39
jeremy haudry <jhaudry@proton.me>
2025-12-24 17:06:45 +0100

    cleanup in init.lua and command duration small improvement on format

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,17 +2,6 @@
 require("config.lazy")
 local telescope = require('telescope.builtin')
 
-
--- INIT PLUGINS
--- require('lualine').setup {
---     options = { 
--- 	theme  = "catppuccin",
--- 	sections = {
--- 	    lualine_a = { { 'filename', path = 2 } }
--- 	}
---     }
--- }
-
 -- KEYMAPS
 vim.keymap.set('n', '<leader>ge', vim.cmd.Ex)
 vim.keymap.set('n', '<leader><Tab>', vim.cmd.bnext, {desc = 'Next buffer'})
@@ -32,35 +21,8 @@ vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 require("catppuccin").setup {
     flavour = "macchiato"
 }
+
 vim.cmd.colorscheme "catppuccin"
-
---nvim-treesitter
-require'nvim-treesitter'.setup {
-  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-  install_dir = vim.fn.stdpath('data') .. '/site'
-}
--- require'nvim-treesitter'.install { 'query', 'markdown', 'markdown_inline', 'lua', 'vim', 'vimdoc', 'javascript', 'typescript', 'scala' }
-
--- vim.api.nvim_create_autocmd('FileType', {
-  --pattern = { 'js', 'lua' },
-  --callback = function() vim.treesitter.start() end,
---})
-
---require'nvim-treesitter.configs'.setup {
-  --ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "scala" },
-  --sync_install = false,
-  --auto_install = false,
-  --highlight = {
-    --enable = true,
-    --additional_vim_regex_highlighting = false,
-  --},
---}
-
--- vim.pack.add{
---   { src = 'https://github.com/nvim-lua/plenary.nvim' },
---   { src = 'https://github.com/nvim-telescope/telescope.nvim' }
--- }l
-
 
 -- vim.api.nvim_set_option("clipboard", "unnamed")
 
@@ -76,24 +38,5 @@ require'nvim-treesitter'.setup {
 --   end,
 --   group = highlight_group,
 --   pattern = '*',
--- })
-
--- -- Dynamic make file program selector
--- local function match(file, pattern)
---   local startPosition, endPosition = string.find(file, pattern)
---   return startPosition ~= nil and endPosition ~= nil
--- end
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "typescript",
---   callback = function(ev)
---     local file = ev.file
---     -- ts tests files
---     if match(file, "%.spec%.") or match(file, "%.test%.") then
---       print("TS test file : " .. file)
---       vim.cmd("compiler jest | setlocal makeprg=npx\\ jest")
---     else
---       vim.cmd("compiler tsc | setlocal makeprg=npx\\ tsc")
---     end
---   end
 -- })
 
